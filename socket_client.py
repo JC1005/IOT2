19d5a118929d717dfbd6a60db4902917c9eb41c8
deriando <deriandogoh@gmail.com>
2020-08-21 22:55:00 +0800

    update: mqtt file path

--- socket_client.py
+++ socket_client.py
@@ -1,39 +1,45 @@
 from time import sleep
 import sys
 import json
-
-from datetime import datetime  
-from datetime import timedelta  
+from aws import awsMQTT
+from datetime import datetime
+from datetime import timedelta
 import argparse
 
 from IOTAssignmentClientdorachua.GrabCarClient import GrabCarClient
 from IOTAssignmentUtilitiesdorachua.MySQLManager import MySQLManager
-from IOTAssignmentUtilitiesdorachua.MySQLManager import QUERYTYPE_DELETE, QUERYTYPE_INSERT
+from IOTAssignmentUtilitiesdorachua.MySQLManager import (
+    QUERYTYPE_DELETE,
+    QUERYTYPE_INSERT,
+)
 
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import json
 
-def getData(gcc,datetime_start,my_rpi):    
+
+def getData(gcc, datetime_start, my_rpi):
     while True:
         try:
-            reading = gcc.get_reading()            
+            reading = gcc.get_reading()
 
-            if reading is not None and len(reading)>0:
+            if reading is not None and len(reading) > 0:
                 readings = json.loads(reading)
-                            
+
                 for str_reading in readings:
                     r = json.loads(str_reading)
                     import uuid
+
                     uuid = uuid.uuid1()
                     from datetime import datetime
-                    now = datetime.now() # current date and time
+
+                    now = datetime.now()  # current date and time
                     datetime_value = now.strftime("%Y-%m-%d %H:%M:%S")
-                    r['id'] = str(uuid)
-                    r['datetime_value'] = datetime_value
+                    r["id"] = str(uuid)
+                    r["datetime_value"] = datetime_value
                     print(r)
                     my_rpi.publish("sensors/speed", json.dumps(r), 1)
-                    
-            yield             
+
+            yield
 
         except GeneratorExit:
             print("Generator Exit error")
@@ -46,29 +52,30 @@ def getData(gcc,datetime_start,my_rpi):
 
         except:
             print(sys.exc_info()[0])
-            print(sys.exc_info()[1])       
+            print(sys.exc_info()[1])
 
 
 if __name__ == "__main__":
 
-    try:                 
-        host,port = "127.0.0.1", 8889
+    try:
+        host, port = "127.0.0.1", 8889
         parser = argparse.ArgumentParser()
-        parser.add_argument('host')
-        parser.add_argument('port',type=int)
-        
+        parser.add_argument("host")
+        parser.add_argument("port", type=int)
+
         args = parser.parse_args()
         if args.host:
             host = args.host
         if args.port:
             port = args.port
 
-        mygcc = GrabCarClient(host,port)
+        mygcc = GrabCarClient(host, port)
+        aws = awsMQTT()
 
-        host = "a360v5qyx1qhmx-ats.iot.us-east-1.amazonaws.com"
-        rootCAPath = "certs/rootca.pem"
-        certificatePath = "certs/certificate.pem.crt"
-        privateKeyPath = "certs/private.pem.key"
+        host = aws.host
+        rootCAPath = aws.rootCAPath
+        certificatePath = aws.certificatePath
+        privateKeyPath = aws.privateKeyPath
 
         my_rpi = AWSIoTMQTTClient("basicPubSub")
         my_rpi.configureEndpoint(host, 8883)
@@ -82,25 +89,20 @@ if __name__ == "__main__":
         # Connect and subscribe to AWS IoT
         my_rpi.connect()
 
-        print("Streaming started")        
+        print("Streaming started")
         datetime_start = datetime.now()
 
-
-        gen = getData(mygcc,datetime_start,my_rpi)
+        gen = getData(mygcc, datetime_start, my_rpi)
 
         while True:
             next(gen)
             sleep(2)
-        
-    except KeyboardInterrupt:
-        print('Interrupted')        
-        sys.exit()
 
+    except KeyboardInterrupt:
+        print("Interrupted")
+        sys.exit()
 
     except:
         print(sys.exc_info()[0])
-        print(sys.exc_info()[1])       
-
-
-
+        print(sys.exc_info()[1])
 
