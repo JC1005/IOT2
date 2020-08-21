ecad913ae56d47638e2536050c405f4c128ffb3c
samxy2612 <66238453+samxy2612@users.noreply.github.com>
2020-08-21 21:20:20 +0800

    added notifcation.py

--- run_socket_server.bat
+++ run_socket_server.bat
@@ -1,4 +1,4 @@
 cd /d %~dp0
 
-cmd.exe /K "conda activate iot_env & python.exe socket_server.py 127.0.0.1 8889"
+cmd.exe /K "conda activate IoT_assign & python.exe socket_server.py 127.0.0.1 8889"
 
