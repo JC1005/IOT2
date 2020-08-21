ffe8d693f69bb47d5cd6488a4f11247b20beea84
deriando <deriandogoh@gmail.com>
2020-08-21 22:37:50 +0800

    add configParser > credentials/twilio.ini
    fix: class __init__

--- notification.py
+++ notification.py
@@ -1,19 +1,43 @@
 from twilio.rest import Client
+import configparser
+from pathlib import Path
 
 
-class notify:
-    def __int__(self, speed):
-        self.speed = speed
-        self.account_sid = "ACbccd3db34b4bff24f34b8506b66f4f18"
-        self.auth_token = "bc9b7fb17ae371aaf3cfd407066ec765"
-        self.client = Client(account_sid, auth_token)
-        self.myhp = "+6593545987"
-        self.twiliohp = "+12034635166"
+class twilio:
+    def __init__(self):
+        config = configparser.ConfigParser()
+        configFilePath = "credentials/twilio.ini"
+        # check if config file is exists
+        # if exist, read and store credentials in variables
+        if Path(configFilePath).exists():
+            try:
+                config.read(configFilePath)
+                account_sid = config["default"]["account_sid"]
+                auth_token = config["default"]["auth_token"]
+                self.twiliohp = config["default"]["twiliohp"]
+                self.client = Client(account_sid, auth_token)
+            except:
+                print(configFilePath + "is corrupted")
+        # if config file don't exist, provide a template at configFilePath
+        else:
+            print(configFilePath + "not found, a template will be provided.")
+            config["default"] = {
+                "account_sid": "paste your account SID here",
+                "auth_token": "paste your authentication token here",
+                "twiliohp": "paste your twilio provided number here",
+            }
+            with open(configFilePath, "w") as configfile:
+                config.write(configfile)
 
-    def alert(self):
-        if self.speed > 50:
+    def alert(self, speed, dest_hp):
+        if speed > 50:
             sms = "Be careful, you are already going at a speed above 50KM/H!!!"
-            message = client.api.account.messages.create(
-                to=self.myhp, from_=self.twiliohp, body=sms
+            message = self.client.api.account.messages.create(
+                to=dest_hp, from_=self.twiliohp, body=sms
             )
+            return message
 
+
+if __name__ == "__main__":
+    twilio = twilio()
+    print(twilio.alert(80, "81234567"))
